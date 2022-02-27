332b6a9af102dcf034b1bb3559f596a99f168c88
quangnv3 <quangnv3@rikkeisoft>
2022-02-27 11:58:53 +0700

    Updates

---
+++ precache-manifest.6649180efd4a870aa63fa8f342c9c35e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "707829720d3c63f992375f451c2cf096",
+    "url": "./index.html"
+  },
+  {
+    "revision": "26b79c21118a4787554e",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "d6e247c930b4eb89b4c0",
+    "url": "./static/js/2.88346ecf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.88346ecf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26b79c21118a4787554e",
+    "url": "./static/js/main.abfc4a0d.chunk.js"
+  },
+  {
+    "revision": "b288ca6f4ce65f5523e8",
+    "url": "./static/js/runtime-main.95c165ba.js"
+  }
+]);
