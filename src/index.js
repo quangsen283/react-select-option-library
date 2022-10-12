0730734e79c8b9ed158c44b150dc2213d14e1316
quangnv3 <quangnv3@rikkeisoft.com>
2022-10-12 09:02:10 +0700

    quang dong

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './styles.module.css'
+import Kaka from './Kaka'
 
 export const ExampleComponent = ({ text, tata }) => {
   return (
@@ -7,6 +8,8 @@ export const ExampleComponent = ({ text, tata }) => {
       <h3>Nguyen Van Quang hoho {tata}</h3>
       <h4>Love Dang</h4>
       <div className={styles.test}>Example Component: {text}</div>
+      <Kaka />
+      <div>Toi muon test thu xem vvv</div>
     </div>
   )
 }
